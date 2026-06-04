cd6d314b2e257da2052c2e8e77ea80507aa94cb2
inwoohub <dlsdndls@naver.com>
2026-06-04 15:27:01 +0900

    [Fix] : Pageble 를 직접 받기 -> 각각 Param 으로 받아서 변환

--- src/main/java/com/bbd/item/adapter/in/web/ItemController.java
+++ src/main/java/com/bbd/item/adapter/in/web/ItemController.java
@@ -12,12 +12,16 @@ import com.bbd.item.domain.model.Category;
 import com.bbd.item.domain.model.Item;
 import com.bbd.item.domain.model.Unit;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -70,6 +74,11 @@ public class ItemController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    /**
+     * PATCH 구분선
+     * ======================================
+     */
+
     @Operation(summary = "상품 이름 수정 API (관리지만 가능)")
     @PatchMapping("/{sku}/name")
     public ResponseEntity<Void> updateName(@NotBlank @PathVariable String sku, @Valid @RequestBody UpdateNameItemRequest req) {
@@ -94,6 +103,11 @@ public class ItemController {
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    /**
+     * GET 구분선
+     * ======================================
+     */
+
     @Operation(summary = "Sku 단건 조회 API")
     @GetMapping("/{sku}")
     public ResponseEntity<Item> getItem(@NotBlank @PathVariable String sku) {
@@ -103,7 +117,14 @@ public class ItemController {
 
     @Operation(summary = "전체 조회 API")
     @GetMapping("/all")
-    public ResponseEntity<List<Item>> getAllItems(Pageable pageable) {
+    public ResponseEntity<List<Item>> getAllItems(
+            @RequestParam(required = false, defaultValue = "0") Integer page,
+            @RequestParam(required = false, defaultValue = "20") Integer size,
+            @RequestParam(required = false, defaultValue = "sku") String sortBy,
+            @RequestParam(required = false, defaultValue = "ASC") String direction
+            ) {
+        Sort.Direction sortDirection = Sort.Direction.fromString(direction);
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, sortBy));
         List<Item> items = itemUseCaseGet.getAll(pageable);
         return ResponseEntity.status(HttpStatus.OK).body(items);
     }
@@ -111,13 +132,18 @@ public class ItemController {
     @Operation(summary = "필터 조회")
     @GetMapping
     public ResponseEntity<List<Item>> getItemsFilter(
-            Pageable pageable,
+            @RequestParam(required = false, defaultValue = "0") Integer page,
+            @RequestParam(required = false, defaultValue = "20") Integer size,
+            @RequestParam(required = false, defaultValue = "sku") String sortBy,
+            @RequestParam(required = false, defaultValue = "ASC") String direction,
             @RequestParam(required = false) Category category,
             @RequestParam(required = false) Boolean active,
             @RequestParam(required = false) Unit unit,
             @RequestParam(required = false) Integer minPrice,
             @RequestParam(required = false) Integer maxPrice
     ) {
+        Sort.Direction sortDirection = Sort.Direction.fromString(direction);
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, sortBy));
         GetItemFilterCommand getItemFilterCommand = new GetItemFilterCommand(category, active, unit, minPrice, maxPrice);
         List<Item> items = itemUseCaseGet.getFilter(pageable, getItemFilterCommand);
         return ResponseEntity.status(HttpStatus.OK).body(items);
@@ -126,9 +152,14 @@ public class ItemController {
     @Operation(summary = "이름으로 조회")
     @GetMapping("/name")
     public ResponseEntity<List<Item>> getItemsContainsName(
-            Pageable pageable,
+            @RequestParam(required = false, defaultValue = "0") Integer page,
+            @RequestParam(required = false, defaultValue = "20") Integer size,
+            @RequestParam(required = false, defaultValue = "sku") String sortBy,
+            @RequestParam(required = false, defaultValue = "ASC") String direction,
             @RequestParam(required = false) String name
     ) {
+        Sort.Direction sortDirection = Sort.Direction.fromString(direction);
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, sortBy));
         GetNameCommand getNameCommand = new GetNameCommand(name);
         List<Item> items = itemUseCaseGet.getName(pageable, getNameCommand);
         return ResponseEntity.status(HttpStatus.OK).body(items);
