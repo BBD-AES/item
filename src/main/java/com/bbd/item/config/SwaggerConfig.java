72409ac42df201a3fc7acdd6deb73f93e59660f9
inwoohub <dlsdndls@naver.com>
2026-06-05 12:44:48 +0900

    [Fix] : 스웨거 주소 추가

--- src/main/java/com/bbd/item/config/SwaggerConfig.java
+++ src/main/java/com/bbd/item/config/SwaggerConfig.java
@@ -19,6 +19,10 @@ public class SwaggerConfig {
                         .description("Local Direct"))
 
                 .addServersItem(new Server()
+                        .url("http://localhost:8082/item")
+                        .description("Local Docker compose"))
+
+                .addServersItem(new Server()
                         .url("http://192.168.200.220/item")
                         .description("Nginx"))
 
