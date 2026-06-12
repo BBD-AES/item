fd09c0a1703fc9ebb2cd0bd1135d397a4956e719
inwoohub <dlsdndls@naver.com>
2026-06-12 12:58:59 +0900

    #33 [Feature] : 엘라스틱서치 의존성 추가 및 ItemDocument 생성

---
+++ src/main/java/com/bbd/item/adapter/out/search/document/ItemSearchDocument.java
@@ -0,0 +1,46 @@
+package com.bbd.item.adapter.out.search.document;
+
+import com.bbd.item.domain.model.item.Item;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Document(indexName = "bbd-items")
+public class ItemSearchDocument {
+
+    @Id
+    private String sku;
+
+    private String name;
+
+    private String category;
+
+    private String unit;
+
+    private int safetyStock;
+
+    private int unitPrice;
+
+    private boolean active;
+
+    private String sourcingType;
+
+    public static ItemSearchDocument from(Item item) {
+        return new ItemSearchDocument(
+                item.getSku(),
+                item.getName(),
+                item.getCategory().name(),
+                item.getUnit().name(),
+                item.getSafetyStock(),
+                item.getUnitPrice(),
+                item.isActive(),
+                item.getSourcingType().name()
+        );
+    }
+}
